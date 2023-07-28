7444ec3bf05683f8944894cf5d2968aae2bfff8f
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-07-28 19:34:45 +0200

    Repair age draw, add people.json to git ignore

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const genders = ['M','F'];
 const maleNames = ['John', 'Michael', 'David', 'James', 'Robert'];
 const femaleNames = ['Mary', 'Jennifer', 'Linda', 'Emily', 'Emma'];
 const lastNames = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones'];
+const outputFile = 'people.json';
 
 function randChoice(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -20,7 +21,7 @@ for (let i = 0; i < 20; i++) {
   const gender = randChoice(genders);
   const firstName = gender === 'M' ? randChoice(maleNames) : randChoice(femaleNames);
   const lastName = randChoice(lastNames);
-  const age = Math.floor(Math.random() * 79) + 18; // Random age between 18 and 57
+  const age = Math.floor(Math.random() * 61) + 18;// Random age between 18 and 57
   const phoneNumber = `+1${Math.floor(Math.random() * 10000000000).toString().padStart(10, '0')}`;
   const email = generateEmail(firstName, lastName);
 
@@ -38,10 +39,10 @@ for (let i = 0; i < 20; i++) {
 
 const jsonData = JSON.stringify(people, null, 2);
 
-fs.writeFile('people.json', jsonData, (err) => {
+fs.writeFile(outputFile, jsonData, (err) => {
   if (err) {
     console.error('Error writing to file:', err);
   } else {
-    console.log('Data saved to people.json successfully!');
+    console.log('Data saved to ' + outputFile + ' successfully!');
   }
 });
